1747f17a0c15846c9e7a8390468f566a3badc17d
dz <812150635@.qq.com>
2020-09-24 21:52:22 +0800

    first commit

---
+++ collection/src/main/java/cn/dz/testgitconnnection.java
@@ -0,0 +1,16 @@
+package cn.dz;
+
+/**
+ * @program: javase
+ * @description:
+ * @author: dz
+ * @create: 2020-09-24 21:45
+ **/
+public class testgitconnnection {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
